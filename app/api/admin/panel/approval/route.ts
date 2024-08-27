22424550f19a0074771086ab080452684f4ea46e
Arjun Dabir <arjunadabir@gmail.com>
2024-08-26 22:35:49 -0700

    fix: no caching on /panel

--- app/api/admin/panel/approval/route.ts
+++ app/api/admin/panel/approval/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectToSupabase from "@/lib/connectToSupabase";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function POST(req: NextRequest) {
-  
+  noStore();
+
   const { id, isApproved } = await req.json();
   const supabase = connectToSupabase();
   const result = await supabase.from("panel").update({ is_approved: isApproved }).eq("id", id);
