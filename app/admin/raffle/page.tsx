6161ddd816e5946b0a40ee686097c786369e6bcc
Arjun Dabir <arjunadabir@gmail.com>
2024-09-27 16:23:23 -0700

    feat: remove cache for raffle

--- app/admin/raffle/page.tsx
+++ app/admin/raffle/page.tsx
@@ -1,8 +1,11 @@
 import RandomGenerator from "@/components/admin/raffle/RandomGenerator";
 import supabase from "@/lib/supabase";
 import React from "react";
+import { unstable_noStore as noStore } from "next/cache";
 
 const page = async () => {
+  noStore();
+
   const { data, error } = await supabase
     .from("users")
     .select("first_name, last_name, raffle_tickets");
