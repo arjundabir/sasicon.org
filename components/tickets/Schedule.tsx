8243ab234f961e6ea2c2f44f2259588d8548b4ac
Arjun Dabir <arjunadabir@gmail.com>
2024-09-28 09:56:36 -0700

    feat: time added

--- components/tickets/Schedule.tsx
+++ components/tickets/Schedule.tsx
@@ -1,44 +1,43 @@
 import React from "react";
-
 const events = [
   {
-    start: "2024-09-28T09:30",
-    end: "2024-09-28T10:10",
+    start: "2024-09-28T16:30:00Z", // 09:30 PST + 7 hours = 16:30 UTC
+    end: "2024-09-28T17:10:00Z", // 10:10 PST + 7 hours = 17:10 UTC
     title: "Check In & Breakfast Refreshments",
   },
   {
-    start: "2024-09-28T10:15",
-    end: "2024-09-28T10:40",
+    start: "2024-09-28T17:15:00Z", // 10:15 PST + 7 hours = 17:15 UTC
+    end: "2024-09-28T17:40:00Z", // 10:40 PST + 7 hours = 17:40 UTC
     title: "Live Table Read and Annotation",
   },
   {
-    start: "2024-09-28T10:50",
-    end: "2024-09-28T11:50",
+    start: "2024-09-28T17:50:00Z", // 10:50 PST + 7 hours = 17:50 UTC
+    end: "2024-09-28T18:50:00Z", // 11:50 PST + 7 hours = 18:50 UTC
     title: "Workshops: 'Develop'",
   },
   {
-    start: "2024-09-28T12:00",
-    end: "2024-09-28T12:45",
+    start: "2024-09-28T19:00:00Z", // 12:00 PST + 7 hours = 19:00 UTC
+    end: "2024-09-28T19:45:00Z", // 12:45 PST + 7 hours = 19:45 UTC
     title: "Lunch",
   },
   {
-    start: "2024-09-28T12:55",
-    end: "2024-09-28T13:35",
+    start: "2024-09-28T19:55:00Z", // 12:55 PST + 7 hours = 19:55 UTC
+    end: "2024-09-28T20:35:00Z", // 13:35 PST + 7 hours = 20:35 UTC
     title: "UCI Administration Q&A",
   },
   {
-    start: "2024-09-28T13:45",
-    end: "2024-09-28T14:45",
+    start: "2024-09-28T20:45:00Z", // 13:45 PST + 7 hours = 20:45 UTC
+    end: "2024-09-28T21:45:00Z", // 14:45 PST + 7 hours = 21:45 UTC
     title: "Workshops: 'Design'",
   },
   {
-    start: "2024-09-28T14:45",
-    end: "2024-09-28T15:45",
+    start: "2024-09-28T21:45:00Z", // 14:45 PST + 7 hours = 21:45 UTC
+    end: "2024-09-28T22:45:00Z", // 15:45 PST + 7 hours = 22:45 UTC
     title: "Workshops: 'Demonstrate'",
   },
   {
-    start: "2024-09-28T16:05",
-    end: "2024-09-28T16:30",
+    start: "2024-09-28T23:05:00Z", // 16:05 PST + 7 hours = 23:05 UTC
+    end: "2024-09-28T23:30:00Z", // 16:30 PST + 7 hours = 23:30 UTC
     title: "Closing Remarks & Raffle",
   },
 ];
